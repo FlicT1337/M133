3789900a1c5eaec2ed8819db8e8a5482307a8b3b
Pascal <pascal.fiechter@gibmit.ch>
2020-06-16 11:05:39 +0200

    calendar stuff

--- calendar.js
+++ calendar.js
@@ -1,9 +1,17 @@
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener('DOMContentLoaded', function() {
   var calendarEl = document.getElementById('calendar');
 
   var calendar = new FullCalendar.Calendar(calendarEl, {
-    plugins: ['dayGrid'],
-    defaultView: 'dayGridWeek',
+    plugins: [ 'timeGrid', 'momentPlugin' ],
+    defaultView: 'timeGridWeek',
+    handleWindowResize: true,
+    height: 'auto',
+    weekends: false,
+    allDaySlot: false,
+    nowIndicator: true,
+    minTime: '07:00:00',
+    maxTime: '18:30:00',
+    
   });
 
   calendar.render();
