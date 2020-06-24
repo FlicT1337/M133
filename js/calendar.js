2d6d675a85fc3860e3f15572ff7f54d5532e8fa3
Pascal <pascal.fiechter@gibmit.ch>
2020-06-24 09:08:56 +0200

    finished

--- js/calendar.js
+++ js/calendar.js
@@ -1,54 +1,55 @@
 //Full Calendar Settings und Event eintragen
-const scheduleUrl = 'https://sandbox.gibm.ch/tafel.php';
-var calendarEl = document.getElementById('calendar');
+const scheduleUrl = "https://sandbox.gibm.ch/tafel.php";
+var calendarEl = document.getElementById("calendar");
 var events = [];
+
 var calendar = new FullCalendar.Calendar(calendarEl, {
-  plugins: ['timeGrid', 'bootstrap'],
-  defaultView: 'timeGridWeek',
+  plugins: ["timeGrid", "bootstrap"],
+  defaultView: "timeGridWeek",
   handleWindowResize: true,
-  height: 'auto',
+  height: "auto",
   weekends: false,
   allDaySlot: false,
   nowIndicator: true,
-  locale: 'de',
-  timeZone: 'UTC+2',
-  minTime: '07:00:00',
-  maxTime: '20:00:00',
-  themeSystem: 'bootstrap',
+  locale: "de",
+  timeZone: "UTC+2",
+  minTime: "07:00:00",
+  maxTime: "20:00:00",
+  themeSystem: "bootstrap",
   footer: false,
   header: {
-    left: 'prevDate, nextDate',
-    center: 'title',
-    right: 'openModal,todayB',
+    left: "prevDate, nextDate",
+    center: "title",
+    right: "openModal,todayB",
   },
   //costum buttons for the calendar
   customButtons: {
     openModal: {
-      text: 'einstellungen',
-      bootstrapFontAwesome: 'fas fa-cog',
+      text: "einstellungen",
+      bootstrapFontAwesome: "fas fa-cog",
       click: function () {
-        $('#myModal').modal('show');
+        $("#myModal").modal("show");
       },
     },
     nextDate: {
-      text: '>',
-      bootstrapFontAwesome: 'fas fa-step-forward',
+      text: ">",
+      bootstrapFontAwesome: "fas fa-step-forward",
       click: function () {
         calendar.next();
         initializeCalendar();
       },
     },
     prevDate: {
-      text: '<',
-      bootstrapFontAwesome: 'fas fa-step-backward',
+      text: "<",
+      bootstrapFontAwesome: "fas fa-step-backward",
       click: function () {
         calendar.prev();
         initializeCalendar();
       },
     },
     todayB: {
-      text: 'today',
-      bootstrapFontAwesome: 'fas fa-calendar-day',
+      text: "today",
+      bootstrapFontAwesome: "fas fa-calendar-day",
       click: function () {
         calendar.today();
         initializeCalendar();
@@ -65,34 +66,33 @@ window.onload = function () {
 function initializeCalendar() {
   events = [];
   var now = moment(calendar.getDate());
-  var week = now.format('ww-yyyy');
-  const url = `${scheduleUrl}?klasse_id=${localStorage.getItem('class')}&woche=${week}`;
+  var week = now.format("ww-yyyy");
+  const url = `${scheduleUrl}?klasse_id=${localStorage.getItem(
+    "class"
+  )}&woche=${week}`;
   $.getJSON(url, function (data) {
     for (var table of data) {
       var event = {
         title: table.tafel_longfach,
-        start: table.tafel_datum + 'T' + table.tafel_von,
-        end: table.tafel_datum + 'T' + table.tafel_bis,
+        start: table.tafel_datum + "T" + table.tafel_von,
+        end: table.tafel_datum + "T" + table.tafel_bis,
         prof: table.tafel_lehrer,
         room: table.tafel_raum,
         comment: table.tafel_kommentar,
         allDay: false,
       };
-      console.log(JSON.stringify(events, null, ''));
+
       events.push(event);
     }
-  })
-    .done(function () {
-      calendar.getEvents().forEach(removeEvents);
-      function removeEvents(eventObject) {
-        eventObject.remove();
-      }
-      calendar.getEvents().forEach((event) => event.remove());
-      events.forEach((event) => calendar.addEvent(event));
-    })
-    .fail(function () {
-      console.log('Data fetch failed');
-    });
+  }).done(function () {
+    calendar.getEvents().forEach(removeEvents);
+    function removeEvents(eventObject) {
+      eventObject.remove();
+    }
+    calendar.getEvents().forEach((event) => event.remove());
+    events.forEach((event) => calendar.addEvent(event));
+  });
+
   //renders the calendar with the events
   calendar.render();
 }
